10cb8126b7d56c0f3857f4f38b8eb8df6e112adc
Qin Geng <gengqin50@gmail.com>
2025-09-22 14:17:27 -0700

    update CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Quiz API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[o.strip() for o in settings.CORS_ORIGINS.split(",")],
+    allow_origins=[o.strip() for o in settings.CORS_ORIGINS],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
